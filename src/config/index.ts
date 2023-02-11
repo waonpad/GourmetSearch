8b338996653a27da8e97c157b0637000f7115ec9
waonpad <waonpad@gmail.com>
2023-02-11 23:05:15 +0900

    0211

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,3 +2,12 @@ export const APP_NAME = process.env.REACT_APP_NAME as string;
 export const API_URL = process.env.REACT_APP_API_URL as string;
 export const APP_GITHUB_REPO = process.env.REACT_APP_GITHUB_REPO as string;
 export const JWT_SECRET = '123456' as string;
+
+export const FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY as string;
+export const FIREBASE_AUTH_DOMAIN = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN as string;
+export const FIREBASE_PROJECT_ID = process.env.REACT_APP_FIREBASE_PROJECT_ID as string;
+export const FIREBASE_STORAGE_BUCKET = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET as string;
+export const FIREBASE_MESSAGING_SENDER_ID = process.env
+  .REACT_APP_FIREBASE_MESSAGING_SENDER_ID as string;
+export const FIREBASE_APP_ID = process.env.REACT_APP_FIREBASE_APP_ID as string;
+export const FIREBASE_MEASUREMENT_ID = process.env.REACT_APP_FIREBASE_MEASUREMENT_ID as string;
